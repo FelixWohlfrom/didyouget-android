d5befea431746e9eb54592149af2876b5e9e70cb
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2023-07-07 20:31:32 +0200

    Optimize implementation of shopping list data loading

--- app/src/main/java/de/wohlfrom/didyouget/data/sources/ShoppingListDataSource.kt
+++ app/src/main/java/de/wohlfrom/didyouget/data/sources/ShoppingListDataSource.kt
@@ -12,44 +12,40 @@ import java.util.LinkedList
 class ShoppingListDataSource {
 
     suspend fun loadShoppingLists(): Result<List<ShoppingList>> {
-        try {
-            val response = try {
-                apolloClient().query(ShoppingListsQuery()).execute()
-            } catch (e: Exception) {
-                Log.e("loadShoppingList", e.toString())
-                null
-            }
+        val response = try {
+            apolloClient().query(ShoppingListsQuery()).execute()
+        } catch (e: Exception) {
+            Log.e("loadShoppingList", e.toString())
+            return Result.Error(IOException("Error fetching shopping list", e))
+        }
 
-            val shoppingListsData = response?.data?.shoppingLists
-            if (shoppingListsData == null || response.hasErrors()) {
-                return Result.Error(Exception(response?.errors?.get(0)?.message ?: "Unknown error"))
-            }
+        val shoppingListsData = response.data?.shoppingLists
+        if (shoppingListsData == null || response.hasErrors()) {
+            return Result.Error(Exception(response.errors?.get(0)?.message ?: "Unknown error"))
+        }
 
-            val shoppingLists = LinkedList<ShoppingList>()
-            for (shoppingListData in shoppingListsData) {
-                val shoppingListItems = LinkedList<ListItem>()
-                for (listItem in shoppingListData!!.listItems ?: LinkedList()) {
-                    shoppingListItems.add(
-                        ListItem(
-                            listItem!!.id,
-                            listItem.value,
-                            listItem.bought,
-                        )
-                    )
-                }
-
-                shoppingLists.add(
-                    ShoppingList(
-                        shoppingListData.id,
-                        shoppingListData.name,
-                        shoppingListItems
+        val shoppingLists = LinkedList<ShoppingList>()
+        for (shoppingListData in shoppingListsData) {
+            val shoppingListItems = LinkedList<ListItem>()
+            for (listItem in shoppingListData!!.listItems ?: LinkedList()) {
+                shoppingListItems.add(
+                    ListItem(
+                        listItem!!.id,
+                        listItem.value,
+                        listItem.bought,
                     )
                 )
             }
-            return Result.Success(shoppingLists)
-        } catch (e: Throwable) {
-            return Result.Error(IOException("Error fetching shopping list", e))
+
+            shoppingLists.add(
+                ShoppingList(
+                    shoppingListData.id,
+                    shoppingListData.name,
+                    shoppingListItems
+                )
+            )
         }
+        return Result.Success(shoppingLists)
     }
 
     suspend fun addShoppingList(name: String): Result<ShoppingList> {
