fd7cf87aeea3cc0e0517934332afd91cde00d0e7
Felix Wohlfrom <FelixWohlfrom@users.noreply.github.com>
2025-07-16 23:11:16 +0200

    Add first navigation test
    ... and cleaned up some unneeded stuff

--- app/src/test/java/de/wohlfrom/didyouget/ui/shoppingList/ShoppingListViewTest.kt
+++ app/src/test/java/de/wohlfrom/didyouget/ui/shoppingList/ShoppingListViewTest.kt
@@ -2,13 +2,18 @@ package de.wohlfrom.didyouget.ui.shoppingList
 
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions.scrollTo
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
 import de.wohlfrom.didyouget.BuildConfig
 import de.wohlfrom.didyouget.R
 import de.wohlfrom.didyouget.data.ShoppingListRepository
@@ -18,8 +23,6 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import io.mockk.mockkConstructor
-import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.equalTo
 import org.junit.Assume.assumeTrue
 import org.junit.Before
 import org.junit.Test
@@ -49,7 +52,7 @@ class ShoppingListFragmentTest {
         scenario.moveToState(Lifecycle.State.RESUMED)
         scenario.moveToState(Lifecycle.State.DESTROYED)
 
-        assertThat(ShadowToast.getTextOfLatestToast(), equalTo(expectedFailureMessage))
+        assertThat(ShadowToast.getTextOfLatestToast()).isEqualTo(expectedFailureMessage)
     }
 
     @Test
@@ -90,5 +93,33 @@ class ShoppingListFragmentTest {
         scenario.moveToState(Lifecycle.State.DESTROYED)
 
         coVerify { ShoppingListRepository(mockk()).loadShoppingLists() }
+    }
+
+    @Test
+    fun testClickOnList() {
+        val resultList = LinkedList<ShoppingList>()
+        resultList.add(ShoppingList("1", "First item"))
+        resultList.add(ShoppingList("2", "Second item"))
+
+        mockkConstructor(ShoppingListRepository::class)
+        coEvery { anyConstructed<ShoppingListRepository>().loadShoppingLists() } returns
+                Result.Success(data = resultList)
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+
+        val scenario = launchFragmentInContainer<ShoppingListFragment>()
+        scenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.navigation_graph)
+            navController.setCurrentDestination(R.id.shoppingListFragment)
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(withId(R.id.list))
+            .perform(scrollTo<RecyclerView.ViewHolder>(hasDescendant(withText("First item"))))
+        onView(withText("First item")).perform(click())
+
+        assertThat(navController.currentDestination?.id).isEqualTo(R.id.shoppingListItemFragment)
+        val currentArgs = navController.backStack.last().arguments
+        assertThat(currentArgs?.getInt("listId")).isEqualTo(0)
+        assertThat(currentArgs?.getString("listName")).isEqualTo("First item")
     }
 }
